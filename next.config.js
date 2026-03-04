62056df6cb4c6b69c44ca790be1237c3559858b9
Kshitiztyagi23 <kshitiztyagi541977@gmail.com>
2026-03-04 20:03:34 +0530

    Configure static export and GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/kshitiz-resume',
+  assetPrefix: '/kshitiz-resume',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
